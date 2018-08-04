52b044e3297d047d18f106b30c3317afccf0854e
Kennedy Mutemi <kennedy@debian.home.net>
2018-08-04 16:25:49 +0300

    [masoko_com_search.py]Use Python's unittest library
    Use unittest framework on this script

--- masoko_com_search.py
+++ masoko_com_search.py
@@ -1,11 +1,33 @@
+import time
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome(executable_path='/opt/chromedriver')
-driver.get("https://www.masoko.com/")
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("huawei")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close
+class SearchText(unittest.TestCase):
+    @classmethod
+    def setUpClass(inst):
+        # create a new Chrome session
+        inst.driver = webdriver.Chrome(executable_path='/opt/chromedriver')
+        inst.driver.implicitly_wait(30)
+        inst.driver.maximize_window()
+        inst.driver.get("https://www.masoko.com/")
+        inst.driver.title
+
+    def test_search(self):
+        # get the search field
+        self.search_field = self.driver.find_element_by_name("q")
+        self.search_field.clear()
+        self.search_field.send_keys("huawei")
+        self.search_field.send_keys(Keys.RETURN)
+
+        # get list of returned data (elements)
+
+        returned_data = self.driver.find_element_by_class_name("product-item")
+        self.assertGreater(0, len(returned_data))
+
+    @classmethod
+    def tearDownClass(inst):
+        # close browser instance
+        inst.driver.quit()
+if __name__ == '__main__':
+    unittest.main()
